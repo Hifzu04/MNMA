32262ec7ce661c2dc97a83baa08bb6f245809388
Your Name <zaidiinsiya83@gmail.com>
2026-05-28 03:25:53 +0530

    register route

--- Backend/routes/userroutes.js
+++ Backend/routes/userroutes.js
@@ -1,1 +1,28 @@
 import express from "express"
+import user from "../models/user.js"
+import jwt from "jsonwebtoken"
+const authroute = express.Router()
+
+authroute.post("/register" , async(req,res)=>{
+    const {name , email , password} = req.body
+    try {
+       
+ let checkuser =  await user.findOne({email})
+ if( checkuser) {
+return res.status(400).json({message:"User already exists"})
+ }
+     checkuser = new user({name , email , password})
+   await checkuser.save()
+   res.status(201).json({
+    userobj:{
+        _id: checkuser._id , name : checkuser.name, email: checkuser.email , role: checkuser.role
+    }
+   })
+} 
+
+    catch (error) {
+        console.log(error)
+        res.status(500).send("Server error")
+    }
+})
+export default authroute
