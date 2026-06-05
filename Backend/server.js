aba99f26d7ec20427c6b8e425396fcd2ab5f4cbb
Your Name <zaidiinsiya83@gmail.com>
2026-06-05 23:17:36 +0530

    set up + upload image to cloudinary

--- Backend/server.js
+++ Backend/server.js
@@ -5,7 +5,7 @@ import connectdb from "./Config/db.js"
 import authroute from "./routes/userroutes.js"
 import productroute from "./routes/productroutes.js"
 import cartroute from "./routes/cartroutes.js"
-
+import { uploadroute } from "./routes/uploadroutes.js"
 dotenv.config()
 
 const app = express()
@@ -18,7 +18,7 @@ connectdb()
 app.use("/api/user", authroute)
 app.use("/api/products", productroute)
 app.use("/api/cart", cartroute)
-
+app.use("/api/upload" ,uploadroute )
 app.get("/", (req, res) => {
     res.send("WELCOME")
 })
