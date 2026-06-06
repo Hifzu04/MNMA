4dfeeac32ad622dd754689c002a305d02ed86276
Faiz Khan <faizurbtech@gmail.com>
2026-06-06 01:29:41 -0700

    Add NewArrivalspage

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from '../Components/Layout/Hero'
 import GenderCollection from '../Components/Products/GenderCollection'
+import NewArrivals from '../Components/Products/NewArrivals'
 
 function Home() {
   return (
     <div>
       <Hero />
       <GenderCollection/>
+      <NewArrivals/>
     </div>
   )
 }
