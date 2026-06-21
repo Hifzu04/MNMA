6eb6b243cde6ddc3e7c74e6184f85a45714c97da
Faiz Khan <faizurbtech@gmail.com>
2026-06-21 10:18:02 -0700

    Add Page of Best Seller on Home Page

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -3,6 +3,7 @@ import Hero from '../Components/Layout/Hero'
 import GenderCollection from '../Components/Products/GenderCollection'
 import NewArrivals from '../Components/Products/NewArrivals'
 import ProductDetail from '../Components/Products/ProductDetail'
+import BestSeller from './BestSeller'
 
 function Home() {
   return (
@@ -11,8 +12,8 @@ function Home() {
       <GenderCollection />
       <NewArrivals />
       {/* bestseller is comming from productDetail.jsx page */}
-      <h2 className='text-3xl font-bold mb-4 text-center'>Best Seller</h2>
-      <ProductDetail />
+    
+      <BestSeller/>
 
 
 
